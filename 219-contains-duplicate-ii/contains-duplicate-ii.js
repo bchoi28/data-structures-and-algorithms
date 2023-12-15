e348116e0dfc8df6137b98c80c2b0fbeb1770d65
Brandon Choi <bchoi28@gmail.com>
2023-12-14 23:22:25 -0500

    Time: 79 ms (84.94%) | Memory: 66.6 MB (38.75%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -14,4 +14,13 @@ var containsNearbyDuplicate = function(nums, k) {
         }
     };
     return false;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+                return true;
+            }
+        }
+    };
+    return false;
 };
