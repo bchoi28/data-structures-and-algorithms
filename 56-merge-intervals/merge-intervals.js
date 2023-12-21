fc8c3b99c1c18c0a16c404522ced4c21e4d99bf7
Brandon Choi <bchoi28@gmail.com>
2023-12-20 22:01:11 -0500

    Time: 89 ms (67.74%) | Memory: 48.1 MB (94.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -3,23 +3,23 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-      intervals.sort((a,b) => a[0] - b[0]);
+   intervals.sort((a,b) => a[0] - b[0]);
   let stack = [intervals[0]];
   for (let i = 1; i < intervals.length; i++) {
 
     let prevInt = {
-      start : stack[stack.length - 1][0],
-      end : stack[stack.length - 1][1]
+      "start" : stack[stack.length - 1][0],
+      "end" : stack[stack.length - 1][1]
     }
 
     let nextInt = {
-      start : intervals[i][0],
-      end : intervals[i][1]
+      "start" : intervals[i][0],
+      "end" : intervals[i][1]
     }
 
-    if (prevInt.end >= nextInt.start && nextInt.end > prevInt.end) {
-      stack[stack.length -1][1] = nextInt.end;
-    } else if (prevInt.end < nextInt.start) {
+    if (prevInt["end"] >= nextInt["start"] && nextInt["end"] > prevInt["end"]) {
+      stack[stack.length -1][1] = nextInt["end"];
+    } else if (prevInt["end"] < nextInt["start"]) {
       stack.push(intervals[i]);
     }
   }
