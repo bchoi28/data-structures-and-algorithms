9f870f7ca581bc791c084a737347607633dd3b97
Brandon Choi <bchoi28@gmail.com>
2023-12-27 20:26:50 -0500

    Time: 136 ms (5.86%) | Memory: 48.7 MB (13.60%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,39 +3,41 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-        const rows = board.length;
+    const rows = board.length;
     const cols = board[0].length;
     let rowMap = {};
     let colMap = {};
     let boxMap = {};
 
     for (let row = 0; row < rows; row++) {
-        rowMap[row] ||= new Set();
-
         for (let col = 0; col < cols; col++) {
-            colMap[col] ||= new Set();
-
             let val = board[row][col];
 
-            if (val !== ".") {
-                if (rowMap[row].has(val)) {
-                    return false;
-                } else {
+            rowMap[row] ||= new Set();
+            if (rowMap[row].has(val)) {
+                return false;
+            } else {
+                if (val !== ".") {
                     rowMap[row].add(val);
                 };
+            };
 
-                if (colMap[col].has(val)) {
-                    return false;
-                } else {
+            colMap[col] ||= new Set();
+            if (colMap[col].has(val)) {
+                return false;
+            } else {
+                if (val !== ".") {
                     colMap[col].add(val);
                 };
+            };
 
-                let boxX = Math.floor(row / 3 );
-                let boxY = Math.floor(col / 3);
-                boxMap[[boxX, boxY]] ||= new Set();
-                if (boxMap[[boxX, boxY]].has(val)) {
-                    return false;
-                } else {
+            let boxX = Math.floor(row / 3 );
+            let boxY = Math.floor(col / 3);
+            boxMap[[boxX, boxY]] ||= new Set();
+            if (boxMap[[boxX, boxY]].has(val)) {
+                return false;
+            } else {
+                if (val !== ".") {
                     boxMap[[boxX, boxY]].add(val);
                 }
             }
